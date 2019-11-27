d597c217b8008cf3754b4deb69472c787a0fc833
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2019-11-26 21:08:49 -0600

    Get NodeMCU working.

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ ESP8266 IoT smart home console
 
 ## BOM
 - NodeMCU V2
+
+## nodemcu
+Deploy to NodeMCU using ESPlorer.
+
+## Heroku Deploy
+Deploy nodemcu-relay submodule with `git subtree push --prefix nodemcu-relay heroku master` from `smart_home_console` directory.
--- nodemcu/application.lua
+++ nodemcu/application.lua
@@ -1,38 +1,66 @@
 local light_control = require("light_control")
 local buttons = require("buttons")
 local http = require("http")
--- local sjson = require("sjson")
+local sjson = require("sjson")
 
 function on_post_done()
     local timer = tmr.create()
-    local switch_state = {}
+    local switch_state = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false}
 
     function do_buttons()
         buttons_pressed = buttons.read_buttons()
-        local changed = false
 
         for i = 1, 16 do
             if buttons_pressed[i] then
-                print("Button Pressed")
-                changed = true
-                switch_state[i] = not switch_state[i]
+                local data
+                if switch_state[i] then
+                    print('Turning off ' ..  ID_MAP[i])
+                    data = '{"id":"' .. ID_MAP[i] .. '","state":"off"}'
+                else
+                    print('Turning on ' ..  ID_MAP[i])
+                    data = '{"id":"' .. ID_MAP[i] .. '","state":"on"}'
+                end
+                http.post(API_ENDPOINT .. "/switches/set",
+                  'Content-Type: application/json\r\n',
+                  data,
+                  function(code, data)
+                    if (code < 0) then
+                      print("HTTP request failed")
+                    else
+                      print(code, data)
+                    end
+                  end)
             end
-        end
-
-        if changed then
-            light_control.set_lights(switch_state)
         end
     end
 
     function query_switches()
       headers = "Authorization: Bearer " .. API_TOKEN .. "\r\n"
-      print(headers)
-      http.get(API_ENDPOINT .. "/switches", headers, function (code, data)
-        -- if code == 200 then
-          -- buttons = sjson.decode(data)
-          -- print(buttons)
-          print(data)
-        -- end
+      http.get(API_ENDPOINT .. "/switches/list", headers, function (code, data)
+        if code == 200 then
+          switches = sjson.decode(data)
+          local changed = false
+
+          for i = 1, #switches do
+            local id = switches[i]['id']
+            local switch = SWITCH_MAP[id]
+            local newstate = (switches[i]['value'] == 'on')
+
+            if switch_state[switch] ~= newstate then
+              if newstate then
+                print(id .. " is now on")
+              else
+                print(id .. " is now off")
+              end
+              switch_state[switch] = newstate
+              changed = true
+            end
+          end
+
+          if changed then
+              light_control.set_lights(switch_state)
+          end
+        end
       end)
 
     end
@@ -41,10 +69,13 @@ function on_post_done()
     switch_state = buttons.read_buttons()
     light_control.set_lights(switch_state)
 
-    timer:register(20, tmr.ALARM_AUTO, do_buttons)
-    timer:start()
+    local button_timer = tmr.create()
+    button_timer:register(20, tmr.ALARM_AUTO, do_buttons)
+    button_timer:start()
 
-    query_switches()
+    local query_timer = tmr.create()
+    query_timer:register(1000, tmr.ALARM_AUTO, query_switches)
+    query_timer:start()
 end
 
 function post()
@@ -69,6 +100,6 @@ function post()
     timer:start()
 end
 
--- light_control.init_lights()
+light_control.init_lights()
 buttons.init_buttons()
 post()
