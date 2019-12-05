d6e24ca068f28bb5833c1c5f960cb5f6db8a768f
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2019-12-04 20:16:44 -0600

    Deploy site.

---
+++ nodemcu-relay/public/precache-manifest.a03625d351e86a98c17ce111613ff3ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b8c77d14d5bc87cf0e988883984dde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c65f6b12491aa4137b7f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c09f878f228b9b4723b7",
+    "url": "/static/js/2.608873a4.chunk.js"
+  },
+  {
+    "revision": "c65f6b12491aa4137b7f",
+    "url": "/static/js/main.2bcc1a76.chunk.js"
+  },
+  {
+    "revision": "427262b6771d3f49a7c5",
+    "url": "/static/js/runtime-main.4aea9da3.js"
+  }
+]);
